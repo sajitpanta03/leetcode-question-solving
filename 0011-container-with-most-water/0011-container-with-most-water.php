f27337f286f5b8319ea1de67c9af4b946890a838
Sajit Panta <52285215+sajitpanta03@users.noreply.github.com>
2026-06-24 15:27:36 +0545

    Time: 32 ms (33.11%), Space: 27.3 MB (19.21%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.php
@@ -0,0 +1,31 @@
+class Solution {
+
+    /**
+     * @param Integer[] $height
+     * @return Integer
+     */
+    function maxArea($height) {
+        $left = 0;
+        $right = count($height) - 1;
+        $maxArea = 0;
+
+        while ($left < $right) {
+
+            $width = $right - $left;
+            $currentHeight = min($height[$left], $height[$right]);
+
+            $maxArea = max(
+                $maxArea,
+                $width * $currentHeight
+            );
+
+            if ($height[$left] < $height[$right]) {
+                $left++;
+            } else {
+                $right--;
+            }
+        }
+
+        return $maxArea;
+    }
+}
