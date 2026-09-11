15a31f720840cb03c8c16d67d23bd716a62049ca
Sajit Panta <52285215+sajitpanta03@users.noreply.github.com>
2026-09-11 10:31:07 +0545

    Time: 13 ms (70.9%), Space: 32.4 MB (22.46%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,15 +1,9 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
 
-        nums.sort()
-
-        for i in range(len(nums) - 1):
-            if nums[i] == nums[i + 1]:
+        for n in nums:
+            if n in hashset:
                 return True
+            hashset.add(n)
         return False
-
-            
